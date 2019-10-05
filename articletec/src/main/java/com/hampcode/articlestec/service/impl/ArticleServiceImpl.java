31e392c54054b956bede6d662b6968b13873d311
Usuario <Usuario>
2019-10-05 11:10:15 -0500

    implementing articleService

--- articletec/src/main/java/com/hampcode/articlestec/service/impl/ArticleServiceImpl.java
+++ articletec/src/main/java/com/hampcode/articlestec/service/impl/ArticleServiceImpl.java
@@ -1,8 +1,9 @@
 package com.hampcode.articlestec.service.impl;
 
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,34 +20,60 @@ public class ArticleServiceImpl implements ArticleService {
 
 	@Override
 	public List<Article> getAllArticles() {
+		List<Article> articles = new ArrayList<Article>();
 		
-		return null;
+		articleRepository.findAll().iterator()
+		                           .forEachRemaining(articles::add);
+		
+		return articles;
 	}
 
 	@Override
 	public Article createArticle(Article article) {
-		return null;
+		Article newArticle;
+		newArticle = articleRepository.save(article);
+		return newArticle;
 	}
 
 	@Override
 	public Article updateArticle(Long id, Article articleDetails) {
-		return null;
+		Article article = findById(id);
+		
+		article.setTitle(articleDetails.getTitle());
+		article.setAuthor(articleDetails.getAuthor());
+		article.setCategory(articleDetails.getCategory());
+		article.setDescription(articleDetails.getDescription());
+		article.setContent(articleDetails.getContent());
+		
+		articleRepository.save(article);
+		
+		return article;
 	}
 
 	@Override
 	public void deleteArticle(Long articleId) {
-		
+		articleRepository.delete(findById(articleId));
 	}
 
 	@Override
 	public Article findById(Long id) {
-		return null;
+		Optional<Article> article = articleRepository.findById(id);
+		
+		//EXCEPTION TODO
+		
+		return article.get();
 	}
 
 	@Override
 	public boolean titleAndAuthorValid(Article article) {
 		
-		return false;
+		List<Article> articles = new ArrayList<>();
+		articleRepository
+			.findByTitleAndAuthor(article.getTitle(), article.getAuthor())
+			.iterator()
+			.forEachRemaining(articles::add);
+	
+		return articles.isEmpty();
 	}
 
 	
