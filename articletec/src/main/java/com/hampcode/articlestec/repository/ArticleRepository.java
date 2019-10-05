efb062015b160d9b199ae76dc316054aeba15dbb
Usuario <Usuario>
2019-10-05 10:11:27 -0500

    changing articleRepository

--- articletec/src/main/java/com/hampcode/articlestec/repository/ArticleRepository.java
+++ articletec/src/main/java/com/hampcode/articlestec/repository/ArticleRepository.java
@@ -2,13 +2,35 @@ package com.hampcode.articlestec.repository;
 
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hampcode.articlestec.model.Article;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends PagingAndSortingRepository<Article, Long> {
+	//crudRepository ->JpaRepository,MongoRepository
+	//jpaRepository ->PagingAndSortingRepository
 
+	//@Query(value="SELECT MAX(id) FROM articles",nativeQuery=true)
+	//Long findTopByOrderByIdDesc();
+	
+	//QUERY("SELECT a FROM Article a WHERE a.author=?1)
+	List<Article> findByAuthor(String author); //esto lo convierte automaticamente
+	
+	@Query("SELECT MAX(id) FROM Article")
+	Long findTopByOrderByIdDesc();
+	
+	@Query("SELECT FROM Article a WHERE a.title=:title and a.author=:author")
+	List<Article> findByTitleAndAuthor(@Param("title")String title,
+			@Param("author")String author);
+	
+	Page<Article> findAll(Pageable pageable); //te devuelve una pagina relacionada con el pageable
 	
 }
